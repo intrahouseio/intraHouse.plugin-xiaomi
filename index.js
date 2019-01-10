ee49ccbff01594f99f6971564cce3685f239fab0
Artem Ivanov <fast0490f@mail.ru>
2019-01-10 16:59:54 +0300

    version 0.0.41

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function getChanelData(sid, props, data, error) {
 }
 
 function start(options) {
-  plugin.debug("version: 0.0.40");
+  plugin.debug("version: 0.0.41");
   const xiaomi = new Xiaomi(options);
   const _commandScan = commandScan.bind({ xiaomi, plugin });
   const _commandRemove = commandRemove.bind({ xiaomi, plugin });
